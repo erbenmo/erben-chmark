11e1c09293f564ed4ca65b00fbaa6267d6f40d11
Erben Mo <moerben@amazon.com>
2013-10-13 23:38:20 -0700

    redis on ec2 wins\!

--- redis/counter.py
+++ redis/counter.py
@@ -7,14 +7,14 @@ def main():
     print r.get('foo')
 
 def test_counter():
-    r = redis.StrictRedis(host='erben-chmark.1rfvi9.0001.usw2.cache.amazonaws.com')
-    #r = redis.StrictRedis(host='ec2-54-200-153-226.us-west-2.compute.amazonaws.com')
+    #r = redis.StrictRedis(host='erben-chmark.1rfvi9.0001.usw2.cache.amazonaws.com')
+    r = redis.StrictRedis(host='ec2-54-200-153-226.us-west-2.compute.amazonaws.com')
     r.set('key', 1)
 
     print datetime.datetime.now()
 
     print r.get('key')
-    for i in range(0, 500):
+    for i in range(0, 5000):
         r.incr('key', 1)
     print r.get('key')
         
